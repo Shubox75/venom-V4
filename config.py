f78799a7f9484f2c99590dc43ef475bcd03002c4
Shubox75 <Shubox677@gmail.com>
2024-07-19 03:14:40 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY VENOMxCRAZY
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("7424479555:AAG21En_vWhghzaUAhKjJ-DoZaLV3KBH4")
+BOT_TOKEN=("7018302949:AAH-Qkd3xrCT2OejC1y-cDtwNighVA2ZGlQ")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("venomXcrazy")
+OWNER_USERNAME=("@Sinzox7")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("5588464519")
+ADMIN_IDS=("6404702674")
 
 
 
